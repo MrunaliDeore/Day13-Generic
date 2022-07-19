3abf4e5b8b8818a4fa1cfe033a01b0e78b93344a
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-07-19 12:13:43 +0530

    Added : findout maximim flaot

--- printMaximum.java
+++ printMaximum.java
@@ -4,33 +4,36 @@ import java.util.Arrays;
 
 public class printMaximum 
 {
-	static int maxint;
-	public static void maximum(int my_array[]) 
+	static double maxdouble;
+	
+	public static void maximum(double my_array[]) 
 	{
-		maxint = my_array[0];
+		maxdouble = my_array[0];
 		int len = my_array.length;
 		for (int i = 1; i < len - 1; i = i + 2) 
 		{
 			if (i + 1 > len) 
 			{
-				if (my_array[i] > maxint) maxint = my_array[i];
+				if (my_array[i] > maxdouble) maxdouble = my_array[i];
 			}
 			if (my_array[i] > my_array[i + 1]) 
 			{
-				if (my_array[i] > maxint) maxint = my_array[i];
+				if (my_array[i] > maxdouble) maxdouble = my_array[i];
 			}
 			if (my_array[i] < my_array[i + 1]) 
 			{
-				if (my_array[i + 1] > maxint) maxint = my_array[i + 1];
+				if (my_array[i + 1] > maxdouble) maxdouble = my_array[i + 1];
 			}
 		}
 	}
+	
+	
 
 	public static void main(String[] args) 
 	{
-		int[] my_array = {25, 14, 56};
+		double[] my_array = {1.5, 5.4, 12.3};
 		maximum(my_array);
 		System.out.println(" Original Array: "+Arrays.toString(my_array));
-		System.out.println(" Maximum value for the above array = " + maxint);
+		System.out.println(" Maximum value for the above array = " + maxdouble);
 	}
 }
